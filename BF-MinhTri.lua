0d4a813bf2a6883f579bb781f7e96b0ca9cdfafe
MinhTri2k13 <nguyentoan01971@gmail.com>
2026-04-04 12:21:42 +0700

    BF-MinhTri.lua

--- BF-MinhTri.lua
+++ BF-MinhTri.lua
@@ -1,11 +1,40 @@
--- MINH TRÍ HUB - BANANA BYPASS
-print("Dang danh lua he thong Banana...")
+-- [[ MINH TRÍ HUB - BẢN QUYỀN RIÊNG CỦA MINH TRÍ ]]
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Fake cái biến môi trường của nó (nếu có)
-getgenv().BananaCat_Loaded = true
+local Window = Rayfield:CreateWindow({
+   Name = "Minh Trí Hub | Blox Fruits",
+   LoadingTitle = "Đang tải Minh Trí Hub...",
+   LoadingSubtitle = "by Minh Trí Gaming",
+   Theme = "Default" -- Có thể đổi thành 'Amber', 'Green', v.v.
+})
 
--- GỌI THẲNG TỪ LINK GỐC CỦA NÓ LUÔN (CHO NÓ KHỎI CHECK)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
+local MainTab = Window:CreateTab("Tự Động Farm", 4483362458) -- Icon farm
 
--- Thông báo của riêng mày
-warn("Minh Tri Hub: Da kich hoat Banana thanh cong!")
+-- Nút bấm Test thử
+MainTab:CreateButton({
+   Name = "Bật Auto Farm Level (Sắp có)",
+   Callback = function()
+       print("Tính năng đang được Minh Trí phát triển!")
+       Rayfield:Notify({
+          Title = "Thông báo",
+          Content = "Mày đợi tao code xong tính năng này đã nhé!",
+          Duration = 5,
+          Image = 4483362458,
+       })
+   end,
+})
+
+local MiscTab = Window:CreateTab("Tiện ích", 4483362458)
+
+MiscTab:CreateButton({
+   Name = "Bay (Fly)",
+   Callback = function()
+       loadstring(game:HttpGet("https://raw.githubusercontent.com/Edgeiy/infiniteyield/master/source"))()
+   end,
+})
+
+Rayfield:Notify({
+   Title = "Thành Công!",
+   Content = "Chào mừng mày đến với Minh Trí Hub!",
+   Duration = 5,
+})

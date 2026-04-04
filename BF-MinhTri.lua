035883bda5875e7e440cd76abe6287f90eed1ec6
MinhTri2k13 <nguyentoan01971@gmail.com>
2026-04-04 12:37:01 +0700

    BF-MinhTri.lua

--- BF-MinhTri.lua
+++ BF-MinhTri.lua
@@ -1,27 +1,10 @@
--- [[ MINH TRÍ HUB - BẢN SIÊU NHẸ CHO PC ]]
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Minh Trí Hub - PC Edition", "Midnight")
+-- [[ MINH TRÍ HUB - BẢN RAW (KHÔNG MENU) ]]
+print("Dang chay ban RAW cho máy UNC thap...")
 
--- Tab chính
-local Tab1 = Window:NewTab("Main Farm")
-local Section1 = Tab1:NewSection("Auto Farm Level")
+-- Bật Bay (Infinite Yield) - Cái này nhẹ nhất thế giới rồi
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Edgeiy/infiniteyield/master/source"))()
 
-Section1:NewButton("Bật Farm (Sắp có)", "Chức năng đang phát triển", function()
-    print("Minh Trí đang code...")
-end)
+-- Chỉnh tốc độ chạy thẳng luôn (Không cần nút bấm)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100
 
--- Tab tiện ích (Dùng mấy cái lệnh đơn giản)
-local Tab2 = Window:NewTab("Tiện ích")
-local Section2 = Tab2:NewSection("Dành cho PC")
-
-Section2:NewSlider("Tốc độ chạy (WalkSpeed)", "Chỉnh tốc độ nhân vật", 250, 16, function(s)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-end)
-
-Section2:NewButton("Bay (Infinite Yield)", "Dùng để phá game", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Edgeiy/infiniteyield/master/source"))()
-end)
-
-Section2:NewButton("Nhập Key Banana", "Dán Key của mày vào bảng gốc", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
-end)
+warn("Minh Tri Hub: Da load xong ban RAW. Neu van loi thi do Executor cua may qua cui!")

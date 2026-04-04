14ffb1a9d5da6de2589d2ba3ef981197fea26742
MinhTri2k13 <nguyentoan01971@gmail.com>
2026-04-04 11:58:38 +0700

    BF-MinhTri.lua

--- BF-MinhTri.lua
+++ BF-MinhTri.lua
@@ -1,2 +1,11 @@
-print("DANG TAI CODE BANANA XIN...")
+-- MINH TRÍ HUB - BANANA BYPASS
+print("Dang danh lua he thong Banana...")
+
+-- Fake cái biến môi trường của nó (nếu có)
+getgenv().BananaCat_Loaded = true
+
+-- GỌI THẲNG TỪ LINK GỐC CỦA NÓ LUÔN (CHO NÓ KHỎI CHECK)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
+
+-- Thông báo của riêng mày
+warn("Minh Tri Hub: Da kich hoat Banana thanh cong!")

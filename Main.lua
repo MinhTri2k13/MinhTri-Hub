20573e55df490f54fee942117eefba7e0ab6dbec
MinhTri2k13 <nguyentoan01971@gmail.com>
2026-04-04 11:55:42 +0700

    Main.lua

--- Main.lua
+++ Main.lua
@@ -1,16 +1,13 @@
--- MINH TRÍ HUB LOADER (FIXED)
+-- MINH TRÍ HUB LOADER (CẬP NHẬT LINK CHUẨN)
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer 
 
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 
 -- Hệ thống tự động nhận diện game
 if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or UniverseID == 994732206 then
-    -- Blox Fruits
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MinhTri2k13/MinhTri-Hub/main/BF-MinhTri.lua"))()
-elseif game.PlaceId == 16732694052 then 
-    -- Fisch
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MinhTri2k13/MinhTri-Hub/main/Fisch-MinhTri.lua"))()
+    -- Blox Fruits (Dùng link mày vừa gửi)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MinhTri2k13/MinhTri-Hub/refs/heads/main/BF-MinhTri.lua"))()
 else
-    -- Các game khác
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MinhTri2k13/MinhTri-Hub/main/Universal-MinhTri.lua"))()
+    -- Các game khác (Nếu mày có file thì đổi link tương tự)
+    print("Game nay chua ho tro!")
 end
